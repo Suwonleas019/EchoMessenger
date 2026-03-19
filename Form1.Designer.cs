5386a4cd18992a6afea8786b3d050f9b0d86ee45
수환 김 <leas019@suwon.ac.kr>
2026-03-19 17:20:57 +0900

    과제1 코딩과 테스트 완료 Label을 좌상단에 배치 후 색깔과 크기 및 글씨체 변경 listbox를 중간에 배치 후 크기변경 textbox를 listbox 하단에 배치 button을 text 상자 우측에 배치하여 클릭하기 편하게 배치

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            BtnEnter = new Button();
+            lbltitle = new Label();
+            Txtchett = new TextBox();
+            LBX = new ListBox();
+            SuspendLayout();
+            // 
+            // BtnEnter
+            // 
+            BtnEnter.BackColor = Color.FromArgb(128, 255, 128);
+            BtnEnter.Font = new Font("바탕체", 18F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            BtnEnter.Location = new Point(634, 338);
+            BtnEnter.Name = "BtnEnter";
+            BtnEnter.Size = new Size(114, 74);
+            BtnEnter.TabIndex = 0;
+            BtnEnter.Text = "전송";
+            BtnEnter.UseVisualStyleBackColor = false;
+            BtnEnter.Click += BtnEnter_Click;
+            // 
+            // lbltitle
+            // 
+            lbltitle.AutoSize = true;
+            lbltitle.Font = new Font("맑은 고딕", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            lbltitle.ForeColor = Color.FromArgb(128, 128, 255);
+            lbltitle.Location = new Point(40, 9);
+            lbltitle.Name = "lbltitle";
+            lbltitle.Size = new Size(286, 46);
+            lbltitle.TabIndex = 1;
+            lbltitle.Text = "Echo Messenger";
+            lbltitle.Click += label1_Click;
+            // 
+            // Txtchett
+            // 
+            Txtchett.Location = new Point(40, 365);
+            Txtchett.Name = "Txtchett";
+            Txtchett.Size = new Size(575, 27);
+            Txtchett.TabIndex = 2;
+            // 
+            // LBX
+            // 
+            LBX.FormattingEnabled = true;
+            LBX.Location = new Point(40, 68);
+            LBX.Name = "LBX";
+            LBX.Size = new Size(737, 264);
+            LBX.TabIndex = 3;
+            LBX.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 255, 192);
             ClientSize = new Size(800, 450);
+            Controls.Add(LBX);
+            Controls.Add(Txtchett);
+            Controls.Add(lbltitle);
+            Controls.Add(BtnEnter);
+            Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button BtnEnter;
+        private Label lbltitle;
+        private TextBox Txtchett;
+        private ListBox LBX;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace EchoMessenger
 {
     public partial class Form1 : Form
@@ -5,6 +7,28 @@ namespace EchoMessenger
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnEnter_Click(object sender, EventArgs e)
+        {
+            string typed_msg;
+            typed_msg = Txtchett.Text;
+            LBX.Items.Add(typed_msg);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
